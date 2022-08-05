4100b145ba63eeff8b0aacb44e5f9e62de721808
Jeremiah <jeugenio103@gmail.com>
2022-08-04 23:54:17 -0700

    final update

--- PyPoll/Solved/main.py
+++ PyPoll/Solved/main.py
@@ -40,6 +40,7 @@ print(f"Diana DeGette: {round(Diane_votes/total_votes *100,3)}% ({Diane_votes})"
 print(f"Raymon Anthony Doane: {round(Raymon_votes/total_votes *100,3)}% ({Raymon_votes})")
 print("-------------------------")
 print(f"Winner: {Winner}")
+print("-------------------------")
 
 
 #Text File Output
@@ -51,6 +52,8 @@ with open(output,'a') as new:
     new.write("\n")
     new.write(f"Total Votes: {total_votes}")
     new.write("\n")
+    new.write("----------------------------")
+    new.write("\n")
     new.write(f"Charles Casper Stockham: {round(Charles_votes/total_votes *100,3)}% ({Charles_votes})")
     new.write("\n")
     new.write(f"Diana DeGette: {round(Diane_votes/total_votes *100,3)}% ({Diane_votes})")
@@ -60,3 +63,5 @@ with open(output,'a') as new:
     new.write("-------------------------")
     new.write("\n")
     new.write(f"Winner: {Winner}")
+    new.write("\n")
+    new.write("-------------------------")
